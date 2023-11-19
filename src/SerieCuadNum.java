60bfef8911c1c687c7deeadc6606fdd174f8ad63
Riamu360 <liamzuniga360@gmail.com>
2023-11-19 17:25:26 -0500

    C3: Guardado de las series 5 y 6

--- src/SerieCuadNum.java
+++ src/SerieCuadNum.java
@@ -1,5 +1,5 @@
 public class SerieCuadNum {
-    /* <b>showSerie </b> muestra la serie   1  4   9   16  25  36  49  641
+    /* <b>showSerie </b> muestra la serie   1  4   9   16  25  36  49  64
      * @param NroTerminos : Numero de terminos para la serie
     */
     public void showSerieFor(int NroTerminos){
