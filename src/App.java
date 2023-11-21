8e00e3da724201b4c8a04c2c3670bdab34c0d98c
dayapt04 <daya.pt04@gmail.com>
2023-11-20 20:22:05 -0500

    C2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el numero de terminos: ");
+        int nroTerminos = sc.nextInt();
+
+        Serie3 oSerie3 = new Serie3(nroTerminos);
+        System.out.println("Serie 3: ");
+        oSerie3.PoliCodeCraftersForS3();
+        oSerie3.PoliCodeCraftersWhileS3();
+        oSerie3.PoliCodeCraftersdoWhileS3();
+        System.out.println();
+
+        System.out.println("Serie 4: ");
+        Serie4 oSerie4 = new Serie4(nroTerminos);
+        oSerie4.PoliCodeCraftersForS4();
+        oSerie4.PoliCodeCraftersWhileS4();
+        oSerie4.PoliCodeCraftersdoWhileS4();
+        System.out.println();
+
+        sc.close();
     }
 }
---
+++ src/Serie3.java
@@ -0,0 +1,59 @@
+public class Serie3 {
+    // S3: 0/1 1/3 1/5 2/7 3/9 5/11 8/13 13/15 ...
+    public int nroTerminos;
+
+    public Serie3(int nroTerminos) {
+        this.nroTerminos = nroTerminos;
+    }
+
+    public void PoliCodeCraftersForS3() {
+        int numerador = 0;
+        int suma = 1;
+        int denominador = 1;
+        int i = 0;
+
+        for (; i < this.nroTerminos; i++) {
+            System.out.print(numerador + "/" + denominador + " ");
+            int tempNumerador = numerador + suma;
+            numerador = suma;
+            suma = tempNumerador;
+            denominador += 2;
+        }
+
+        System.out.println();
+    }
+
+    public void PoliCodeCraftersWhileS3() {
+        int numerador = 0;
+        int suma = 1;
+        int denominador = 1;
+        int i = 0;
+
+        while (i < nroTerminos) {
+            System.out.print(numerador + "/" + denominador + " ");
+            int tempNumerador = numerador + suma;
+            numerador = suma;
+            suma = tempNumerador;
+            denominador += 2;
+            i++;
+        }
+
+        System.out.println();
+    }
+
+    public void PoliCodeCraftersdoWhileS3() {
+        int numerador = 0;
+        int suma = 1;
+        int denominador = 1;
+        int i = 0;
+
+        do {
+            System.out.print(numerador + "/" + denominador + " ");
+            int tempNumerador = numerador + suma;
+            numerador = suma;
+            suma = tempNumerador;
+            denominador += 2;
+            i++;
+        } while (i < nroTerminos);
+    }
+}
