8b6f299f270f6e131cd65959b920c714d8205428
dayapt04 <daya.pt04@gmail.com>
2023-12-03 21:21:06 -0500

    merge LiamZ2

--- src/Loading3_4.java
+++ src/Loading3_4.java
@@ -1,65 +1,73 @@
+import java.util.Scanner;
+
 public class Loading3_4 {
     /*
-     * <b>PoliCodeCraftersL3 </b> muestra un caracter que se desplaza de izquierda a derecha en una barra es de 20 caracteres
+     * <b>PoliCodeCraftersL3 </b> muestra un caracter que se desplaza de izquierda a
+     * derecha en una barra es de 20 caracteres
      * 
      * @param charDig: Caracter ingresado
      */
-    public void PoliCodeCraftersForL3(String charDig)throws InterruptedException{
+    Scanner sc = new Scanner(System.in);
+
+    public void PoliCodeCraftersForL3(String charDig) throws InterruptedException {
+        System.out.print("Ingrese un caracter: ");
+        charDig = sc.next();
         System.out.print("[");
-            for(int i = 1; i<21; i++){
-                System.out.print(" ");
-            }
-            System.out.print("]\r");
-        for(int j = 1; j<21; j++){
+        for (int i = 1; i < 21; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("]\r");
+        for (int j = 1; j < 21; j++) {
             System.out.print("[");
-            for(int k = 1; k<21; k++){
-                if(k==j){
+            for (int k = 1; k < 21; k++) {
+                if (k == j) {
                     System.out.print(charDig);
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
-            System.out.print("] "+(j*5)+"%\r");
+            System.out.print("] " + (j * 5) + "%\r");
             Thread.sleep(100);
         }
-        for(int l = 20; l>=1; --l){
+        for (int l = 20; l >= 1; --l) {
             System.out.print("[");
-            for(int m = 1; m<21; m++){
-                if(m==l){
+            for (int m = 1; m < 21; m++) {
+                if (m == l) {
                     System.out.print(charDig);
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
-            if(l*5==100){
-                System.out.print("] "+(l*5)+"%\r");
-            } else if(l*5>=10 && l*5<=95){
-                System.out.print("]  "+(l*5)+"%\r");
-            } else{
-                System.out.print("]   "+(l*5)+"%\r");
+            if (l * 5 == 100) {
+                System.out.print("] " + (l * 5) + "%\r");
+            } else if (l * 5 >= 10 && l * 5 <= 95) {
+                System.out.print("]  " + (l * 5) + "%\r");
+            } else {
+                System.out.print("]   " + (l * 5) + "%\r");
             }
             Thread.sleep(100);
         }
     }
 
     /*
-     * <b>PoliCodeCraftersL4 </b> muestra un waiting que inicia en 0  a 100% usar los signos o0o para simular un movimiento de ida y vuelta en el mismo puesto
+     * <b>PoliCodeCraftersL4 </b> muestra un waiting que inicia en 0 a 100% usar los
+     * signos o0o para simular un movimiento de ida y vuelta en el mismo puesto
      * 
-     * @param 
+     * @param
      */
-    public void PoliCodeCraftersForL4() throws InterruptedException{
+    public void PoliCodeCraftersForL4() throws InterruptedException {
         System.out.println("Loading 4: ");
-        for (int i=0; i<=100; i++){
-            int nSpacios = 2-(i%3);
+        for (int i = 0; i <= 100; i++) {
+            int nSpacios = 2 - (i % 3);
             String bStatus = "";
-            for (int j = 0; j<nSpacios; j++){
+            for (int j = 0; j < nSpacios; j++) {
                 bStatus += "o";
             }
             bStatus += "0";
-            for (int j = 0; j<2-nSpacios; j++){
+            for (int j = 0; j < 2 - nSpacios; j++) {
                 bStatus += "o";
             }
-            System.out.print(bStatus+" "+i+"%\r");
+            System.out.print(bStatus + " " + i + "%\r");
             Thread.sleep(200);
         }
     }
